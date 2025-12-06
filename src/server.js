4310f72f08de8d5993eda1f5ad2e479626329f2a
Mohammad Ali <mohammedali88870@gmail.com>
2025-12-06 17:22:48 +0530

    resolved cors error

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 connectDB();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173", "https://todo-frontend-flax-one.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
